e8be12b9e918cdbc8d7eadf02cc6a2a65485bc3d
Philipp Meier <github@philipp.meier.name>
2012-08-22 16:59:26 +0200

    Test that different way to define resource give same output

--- src/liberator/builder.clj
+++ src/liberator/builder.clj
@@ -4,7 +4,7 @@
   (:use clojure.tools.trace
         midje.sweet))
 
-(defn is-request-method [methods context]
+(defn is-request-method [methods]
   #(some #{(get-in % [:request :request-method])} methods))
 
 (defn wrap-fn
@@ -14,18 +14,19 @@
      (f x)))
 
 (defmacro dedfn [name doc-string f & [args]]
+  (assert (vector? args))
   `(do (defn ~name  ~doc-string [~@args]
-         [~(keyword name)
-          ~(if args
-             `#(~f ~@args %)
-             (wrap-fn f))])
+         [~(keyword name) 
+          (if (fn? (first ~args))
+             (first ~args)
+             (~f ~@args))])
        (alter-meta! #'~name update-in [:arglists] conj [(symbol "function")])))
 
-(dedfn service-available? "accepts function on context or a boolean value" wrap-fn [value])
+(dedfn service-available? "accepts function on context or a boolean value" wrap-fn [bool])
 (dedfn known-method?      "accepts function on context or list of known methods" is-request-method [methods])
-(dedfn uri-too-long?      "accepts function on context or a boolean value" wrap-fn [value])
+(dedfn uri-too-long?      "accepts function on context or a boolean value" wrap-fn [bool])
 (dedfn method-allowed?    "accepts function on context or list of allowed methods " is-request-method [methods])
-(dedfn malformed?         "accepts function on context or a boolean value" wrap-fn [value])
+(dedfn malformed?         "accepts function on context or a boolean value" wrap-fn [malformed])
 
 (defn negotiate-media-type [types]
   (fn [{:keys [request resource] :as context}]
@@ -35,8 +36,6 @@
       {:representation {:media-type (liberator.conneg/stringify type)}})))
 
 (dedfn media-type-available? "accepts function context or list of available media types" negotiate-media-type [media-types])
-(alter-meta! #'media-type-available? #(assoc % :arglists '([media-types] [function])))
-
 (defn negotiate-language [languages]
    (fn [{:keys [request resource] :as context}]
     (when-let [language (liberator.conneg/best-allowed-language
@@ -45,12 +44,16 @@
       {:representation {:language language}})))
 
 (dedfn language-available? "accepts function context or list of available languages" negotiate-language [languages])
-(alter-meta! #'language-available? #(assoc % :arglists '([languages] [function])))
 
 (dedfn handle-ok
        "accepts a function generating a ring response of a constant value that is coerced into
         a ring response"
-       wrap-fn [value])
+       wrap-fn [response])
+
+(dedfn handle-not-acceptable
+       "accepts a function generating a ring response of a constant value that is coerced into
+        a ring response"
+       wrap-fn [response])
 
 (defn resource [& decisions]
   (let [m (into {} decisions)
@@ -64,51 +67,67 @@
     s))
 
 ;; canonical example. every decision is defined as a pair name-function
+
+
+(defn ok [{{media-type :media-type :as representation} :representation}]
+  (as-media-type
+   (str  "The negotiated representation parameters are "
+         representation)
+   media-type))
+
+(defn na [{:keys [represenation request]}]
+  (str "No representation available for the requested attributes: "
+       (select-keys (:headers request)
+                    ["accept" "accept-language"])) )
+
+(def r (resource [:service-available?
+                  (fn [ctx]
+                    (not= (get-in ctx [:request :url]) "/disabled"))]
+                 [:known-method?
+                  (fn [{{m :request-method} :request}]
+                    (some #{m} [:get :put :post :option :trace]))]
+                 [:media-type-available?
+                  (fn [{{{a "accept"} :headers} :request :as ctx}]
+                    ;; todo make core/media-type-available work on f also,
+                    ;; not only on list of media type
+                    (if-let [t (conneg/best-allowed-content-type
+                                a ["text/html" "text/plain"])]
+                      (assoc-in ctx [:representation :media-type] (conneg/stringify t))))]
+                 [:language-available?
+                  (fn [{{{al "accept-language"} :headers} :request}]
+                    (conneg/best-allowed-language al ["de" "en"]))]
+                 [:handle-ok ok]
+                 [:handle-not-acceptable na]))
 (fact
-  (let [r (resource [:service-available?
-                     (fn [ctx]
-                       (not= (get-in ctx [:request :url]) "/disabled"))]
-                    [:known-method?
-                     (fn [{{m :request-method} :request}]
-                       (some #{m} [:get :put :post :option :trace]))]
-                    [:media-type-available?
-                     (fn [{{{a "accept"} :headers} :request :as ctx}]
-                       ;; todo make core/media-type-available work on f also,
-                       ;; not only on list of media type
-                       (if-let [t (conneg/best-allowed-content-type
-                                   a ["text/html" "text/plain"])]
-                         (assoc-in ctx [:representation :media-type] (conneg/stringify t))))]
-                    [:language-available?
-                     (fn [{{{al "accept-language"} :headers} :request}]
-                       (conneg/best-allowed-language al ["de" "en"]))]
-                    [:handle-ok
-                     (fn [{{media-type :media-type :as representation} :representation}]
-                       (as-media-type
-                        (str  "The negotiated representation parameters are "
-                              representation)
-                        media-type))]
-                    [:handle-not-acceptable
-                     (fn [{:keys [represenation request]}]
-                       (str "No representation available for the requested attributes: "
-                            (select-keys (:headers request)
-                                         ["accept" "accept-language"])))])]
-    (r {:request-method :get})
-    => {:body "The negotiated representation parameters are {}", :headers {"Content-Type" "text/plain"}, :status 200}
-    (r {:request-method :get :headers {"accept" "text/plain"}})
-    => {:body "The negotiated representation parameters are {:media-type \"text/plain\"}",
-        :headers {"Content-Type" "text/plain" "Vary" "Accept"}, :status 200}
-    (r {:request-method :get :headers {"accept" "text/rtf"}})
-    => {:body "No representation available for the requested attributes: {\"accept\" \"text/rtf\"}",
-        :headers {"Content-Type" "text/plain"}
-        :status 406}))
+  (r {:request-method :get})
+  => (contains {:body "The negotiated representation parameters are {}", :status 200 })
+
+  (r {:request-method :get :headers {"accept" "text/plain"}})
+  => (contains {:body "The negotiated representation parameters are {:media-type \"text/plain\"}",
+                :headers {"Content-Type" "text/plain" "Vary" "Accept"}, :status 200})
+
+  (r {:request-method :get :headers {"accept" "text/rtf"}})
+  => (contains {:body "No representation available for the requested attributes: {\"accept\" \"text/rtf\"}",
+                :status 406}))
 
 ;; make use of the syntactical helper
 (def r2 (resource (service-available?
                    (fn [ctx] (not= (get-in ctx [:request :uri]) "/disabled")))
-                  (known-method? [:get :put :post :option :trace])
-                  (media-type-available? ["text/html" "text/plain"])
-                  (language-available? ["de"])
-                  (handle-ok #(str "The negotiated representation parameters are " (:representation %)))))
+                  (known-method?
+                   (fn [{{m :request-method} :request}]
+                     (some #{m} [:get :put :post :option :trace])))
+                  (media-type-available?
+                   (fn [{{{a "accept"} :headers} :request :as ctx}]
+                     ;; todo make core/media-type-available work on f also,
+                     ;; not only on list of media type
+                     (if-let [t (conneg/best-allowed-content-type
+                                 a ["text/html" "text/plain"])]
+                       (assoc-in ctx [:representation :media-type] (conneg/stringify t)))))
+                  (language-available?
+                   (fn [{{{al "accept-language"} :headers} :request}]
+                     (conneg/best-allowed-language al ["de" "en"])))
+                  (handle-ok ok)
+                  (handle-not-acceptable na)))
 
 
 ;; make use of conversion to function
@@ -117,4 +136,17 @@
                   (known-method? [:get :put :post :option :trace])
                   (media-type-available? ["text/html" "text/plain"])
                   (language-available? ["de"])
-                  (handle-ok #(str "The negotiated representation parameters are " (:representation %)))))
+                  (handle-ok ok)
+                  (handle-not-acceptable na)))
+
+
+(facts
+  (let [req {:request-method :get}]
+    (fact (r req) => (r2 req))
+    (fact (r req) => (r3 req)))
+  (let [req {:request-method :get :headers {"accept" "text/plain"}}]
+    (fact (r req) => (r2 req))
+    (fact (r req) => (r3 req)))
+  (let [req  {:request-method :get :headers {"accept" "text/rtf"}}]
+    (fact (r req) => (r2 req))
+    (fact (r req) => (r3 req))))
